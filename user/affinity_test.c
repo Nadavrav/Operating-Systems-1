d1c1c0bb18e296abb71c19e0b3ede72d854aa5c5
Nadavrav <nadavrav@post.bgu.ac.il>
2024-06-08 21:52:42 +0300

    Final Final fixes

--- user/affinity_test.c
+++ user/affinity_test.c
@@ -6,7 +6,8 @@ int
 main(int argc, char *argv[])
 {
 
-  set_affinity_mask(5); 
+  //set_affinity_mask(5); 
+  set_affinity_mask((1 << 2) + (1 << 0)); 
   for(;;)
     printf("\nCurrent Process id: %d\n", getpid());
 
